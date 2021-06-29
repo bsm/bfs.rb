fff69a5b6895d951f17586845133d32e67ad0547
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2021-06-29 12:06:28 +0100

    Use Github compatible image

--- sftp/spec/bfs/bucket/sftp_spec.rb
+++ sftp/spec/bfs/bucket/sftp_spec.rb
@@ -2,17 +2,17 @@ require 'spec_helper'
 
 RSpec.describe BFS::Bucket::SFTP, sftp: true do
   subject do
-    described_class.new '127.0.0.1', port: 7023, user: 'user', password: 'pass', prefix: "upload/#{SecureRandom.uuid}"
+    described_class.new '127.0.0.1', port: 7023, user: 'sftp', password: 'DockerPassSFTP', prefix: SecureRandom.uuid
   end
 
   it_behaves_like 'a bucket', content_type: false, metadata: false
 
   it 'resolves from URL' do
-    bucket = BFS.resolve('sftp://user:pass@127.0.0.1:7023')
+    bucket = BFS.resolve('sftp://sftp:DockerPassSFTP@127.0.0.1:7023')
     expect(bucket).to be_instance_of(described_class)
     bucket.close
 
-    bucket = BFS.resolve('sftp://user:pass@127.0.0.1:7023/a/b/')
+    bucket = BFS.resolve('sftp://sftp:DockerPassSFTP@127.0.0.1:7023/a/b/')
     expect(bucket).to be_instance_of(described_class)
     bucket.close
   end
